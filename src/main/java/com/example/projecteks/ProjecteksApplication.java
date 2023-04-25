125d75ee38209602f02c9525177a753e9fb5c2b6
AmingYee <wenm0007@stud.kea.dk>
2023-04-25 10:26:17 +0200

    initial commit

---
+++ src/main/java/com/example/projecteks/ProjecteksApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecteks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecteksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecteksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projecteks/ProjecteksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecteks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecteksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
