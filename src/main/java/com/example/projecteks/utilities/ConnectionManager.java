908a9117b037b8e1ad85c3ecdab33293d1418573
YmerA38 <stin166c@stud.kea.dk>
2023-04-27 14:00:24 +0200

    Med basic css style. Tæt på at være MVP Kan ikke køre med database på azure (vil have project_id)

--- src/main/java/com/example/projecteks/utilities/ConnectionManager.java
+++ src/main/java/com/example/projecteks/utilities/ConnectionManager.java
@@ -9,6 +9,10 @@ public class ConnectionManager {
 
     public static Connection getConnection(){
 
+        /*String url = "jdbc:mysql://eksamen.mysql.database.azure.com";
+        String username = "user";
+        String password = "!test1234";
+*/
         String url = "jdbc:mysql://localhost:3306/Projectmanagement";
         String username = "testuser";
         String password = "Sivertsen13";
