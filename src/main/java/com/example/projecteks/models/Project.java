7062fb4301c6f46fcc284b0d9d2a9da5c699f006
yuyingzhang <yuyi0011@stud.kea.dk>
2023-05-01 14:51:47 +0200

    Project class updated

--- src/main/java/com/example/projecteks/models/Project.java
+++ src/main/java/com/example/projecteks/models/Project.java
@@ -3,8 +3,28 @@ package com.example.projecteks.models;
 public class Project {
     private String projectName;
     private int projectId;
+    private String deadline;
+    private  String subProjectName;
 
     public Project() {}
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getSubProjectName() {
+        return subProjectName;
+    }
+
+    public void setSubProjectName(String subProjectName) {
+        this.subProjectName = subProjectName;
+    }
+
+
 
     public String getProjectName() {
         return projectName;
