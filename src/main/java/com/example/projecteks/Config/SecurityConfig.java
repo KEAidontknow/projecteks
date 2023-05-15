2c3a4e643ba8d4f1f5b0368bb399bfefd818227b
AmingYee <wenm0007@stud.kea.dk>
2023-05-15 09:16:20 +0200

    test

--- src/main/java/com/example/projecteks/Config/SecurityConfig.java
+++ src/main/java/com/example/projecteks/Config/SecurityConfig.java
@@ -1,31 +1,21 @@
 package com.example.projecteks.Config;
 
-import com.example.projecteks.reposetory.Database;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
-import static org.springframework.security.config.Customizer.*;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
 
     @Autowired
     private DataSource dataSource;
@@ -60,7 +50,6 @@ public class SecurityConfig {
                 )
                 .headers(headers -> headers.frameOptions().sameOrigin())
                 .formLogin()
-                .defaultSuccessUrl("/showProject", true)
                 .and()
                 .logout().permitAll()
                 .logoutSuccessUrl("/logOutSuccess")
