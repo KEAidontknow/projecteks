941863af30e56ba2ea41770cc346af4d6613fbfb
AmingYee <wenm0007@stud.kea.dk>
2023-05-23 18:41:27 +0200

    TODO done

--- src/main/java/com/example/projecteks/controllers/LoginController.java
+++ src/main/java/com/example/projecteks/controllers/LoginController.java
@@ -17,6 +17,6 @@ public class LoginController {
             return "users/login";
         }
 
-        return "redirect:/userSite"; //Todo "1" er hardcodet, og skal substitueres med den brugerID på den der rent faktisk er logget ind
+        return "redirect:/userSite";
     }
 }
