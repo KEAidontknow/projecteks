5ab8a50629392f5f196f8c306c3e1ada8c41e99a
YmerA38 <stin166c@stud.kea.dk>
2023-05-26 23:24:59 +0200

    gantt in myAssignmet

--- src/main/java/com/example/projecteks/controllers/AssignmentController.java
+++ src/main/java/com/example/projecteks/controllers/AssignmentController.java
@@ -7,6 +7,7 @@ import com.example.projecteks.reposetory.DatabaseInterface;
 import com.example.projecteks.utilities.DateGenerator;
 import com.example.projecteks.utilities.TimeCalc;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -47,6 +48,8 @@ public class AssignmentController {
     @GetMapping("/myAssignment")
     public String getAssignmentByUserId(Model model) throws RuntimeException{
         model.addAttribute("list", database.getAssignedTasks());
+        model.addAttribute("objectList", database.getAssignmentsByUserName(SecurityContextHolder.getContext().getAuthentication().getName()));
+        model.addAttribute("dateDTOList", DateGenerator.getDateDTOList(0));
         return "Assignment/myAssignment";
     }
     /*@GetMapping("/getAssigmentByTaskId/{projectId}")
